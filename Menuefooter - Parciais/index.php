aa60b972af964e62fe397d5f947a252837be9b1c
Bryan <bryanferreiradosreisferreira@gmail.com>
2025-04-25 12:08:03 -0300

    Add files via upload

---
+++ Menuefooter - Parciais/index.php
@@ -0,0 +1,8 @@
+<?php include '../menuefooter-parciais/Parciais/header.php'; ?>
+
+<main>
+    <h1>Boas vindas ao site!</h1>
+    <p>Esse é um conteúdo de exemplo.</p>
+</main>
+
+<?php include '../menuefooter-parciais/Parciais/footer.php'; ?>
