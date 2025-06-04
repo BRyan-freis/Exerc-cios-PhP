60e4287af18d99a99dc1dd6adce733b60bf3c088
Bryan <bryanferreiradosreisferreira@gmail.com>
2025-06-04 10:43:09 -0300

    Add files via upload

---
+++ Parciais-Responsividade/PARCIAIS/footer.php
@@ -0,0 +1,24 @@
+<link rel="stylesheet" href="../PARCIAIS/CSS/styles.css">
+<footer class="site-footer">
+    <div class="footer-container">
+        <div class="footer-logo">
+            <a href="../HTML/1-index.php"> BlueSite </a>
+        </div>
+        <div class="footer-links">
+            <a href="../HTML/1-index.php"><i class="fas fa-home"></i>Home</a>
+            <a href="../HTML/2-Acessibilidade.php"><i class="fas fa-hand"></i> Acessibilidade</a>
+            <a href="../HTML/3-Seo.php"><i class="fas fa-gear"></i> SEO</a>
+            <a href="../HTML/4-HospedagemG.php"><i class="fas fa-hotel"></i> Hospedagens Gratuitas</a>
+            <a href="../HTML/5-HospedagemP.php"><i class="fas fa-hotel"></i> Hospedagens Pagas</a>
+        </div>
+
+        <div class="footer-social">
+            <a href="https://facebook.com" target="_blank" aria-label="Facebook"> <i class="fab fa-facebook"></i></a>
+            <a href="https://linkedin.com" target="_blank" aria-label="Linkedin"> <i class="fab fa-linkedin-in"></i></a>
+            <a href="https://instagram.com" target="_blank" aria-label="Instagram"> <i class="fab fa-instagram"></i></a>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <p>&copy; <?php echo date("Y"); ?> BlueSite. Todos os direitos reservados.
+        </p>
+    </div>
---
+++ Parciais-Responsividade/PARCIAIS/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../PARCIAIS/CSS/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <title>Header</title>
+</head>
+
+<body>
+    <header>
+        <div class="logo"><a href="../HTML/1-index.php"></a>Home</div>
+        <nav>
+            <ul class="menu">
+                <li><a href="../HTML/2-Acessibilidade.php">Acessibilidade Cognitiva para sites</a></li>
+                <li><a href="../HTML/3-Seo.php">SEO e Ranqueamento</a></li>
+                <li><a href="../HTML/4-HospedagemG.php">Hospedagem Gratuita</a></li>
+                <li><a href="../HTML/5-HospedagemP.php">Hospedagem Paga</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+</html>
